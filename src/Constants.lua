0a705ab326054bffb28286a8a8e5f0aff8df3e42
Robert Machmer <robert.machmer@gmail.com>
2015-08-07 11:38:22 +0200

    Refactor Constants.lua

--- src/Constants.lua
+++ src/Constants.lua
@@ -1,8 +1,6 @@
-local Constants = {};
+local TILE_SIZE  = 16;
 
-Constants.TILE_SIZE  = 16;
-
-Constants.DIRECTION = {
+local DIRECTION = {
     NORTH = 'n',
     SOUTH = 's',
     EAST  = 'e',
@@ -11,12 +9,12 @@ Constants.DIRECTION = {
 
 -- The factions to which the actors can belong. The factions are created from
 -- the player's POV so allied means all actors which are friendly to the player.
-Constants.FACTIONS = {
+local FACTIONS = {
     ALLIED = 'allied',
     ENEMY  = 'enemy',
 }
 
-Constants.ACTOR_TYPES = {
+local ACTOR_TYPES = {
     PLAYER  = 'player',
     CAT     = 'cat',
     TURTLE  = 'turtle',
@@ -25,7 +23,7 @@ Constants.ACTOR_TYPES = {
     VAMPIRE = 'vampire',
 }
 
-Constants.TILE_TYPES = {
+local TILE_TYPES = {
     WALL = 'wall',
     FLOOR = 'floor',
     DOOR = 'door',
@@ -33,32 +31,42 @@ Constants.TILE_TYPES = {
     DOORCLOSED = 'doorclosed',
 }
 
-Constants.ACTOR_STATS = {
-    [Constants.ACTOR_TYPES.PLAYER]  = { speed = 8 },
-    [Constants.ACTOR_TYPES.CAT]     = { speed = 6 },
-    [Constants.ACTOR_TYPES.TURTLE]  = { speed = 1 },
-    [Constants.ACTOR_TYPES.TROLL]   = { speed = 2 },
-    [Constants.ACTOR_TYPES.GOBLIN]  = { speed = 4 },
-    [Constants.ACTOR_TYPES.VAMPIRE] = { speed = 8 },
+local ACTOR_STATS = {
+    [ACTOR_TYPES.PLAYER]  = { speed = 8 },
+    [ACTOR_TYPES.CAT]     = { speed = 6 },
+    [ACTOR_TYPES.TURTLE]  = { speed = 1 },
+    [ACTOR_TYPES.TROLL]   = { speed = 2 },
+    [ACTOR_TYPES.GOBLIN]  = { speed = 4 },
+    [ACTOR_TYPES.VAMPIRE] = { speed = 8 },
 }
 
-Constants.TILE_SPRITES = {
+local TILE_SPRITES = {
     -- Map Tiles
-    [Constants.TILE_TYPES.WALL]       = '#',
-    [Constants.TILE_TYPES.FLOOR]      = '.',
-    [Constants.TILE_TYPES.DOOROPEN]   = 'O',
-    [Constants.TILE_TYPES.DOORCLOSED] = '/',
+    [TILE_TYPES.WALL]       = '#',
+    [TILE_TYPES.FLOOR]      = '.',
+    [TILE_TYPES.DOOROPEN]   = 'O',
+    [TILE_TYPES.DOORCLOSED] = '/',
 
     -- Actor Tiles
-    [Constants.ACTOR_TYPES.PLAYER]  = '@',
-    [Constants.ACTOR_TYPES.CAT]     = 'c',
-    [Constants.ACTOR_TYPES.TURTLE]  = 't',
-    [Constants.ACTOR_TYPES.TROLL]   = 'T',
-    [Constants.ACTOR_TYPES.GOBLIN]  = 'G',
-    [Constants.ACTOR_TYPES.VAMPIRE] = 'V'
+    [ACTOR_TYPES.PLAYER]  = '@',
+    [ACTOR_TYPES.CAT]     = 'c',
+    [ACTOR_TYPES.TURTLE]  = 't',
+    [ACTOR_TYPES.TROLL]   = 'T',
+    [ACTOR_TYPES.GOBLIN]  = 'G',
+    [ACTOR_TYPES.VAMPIRE] = 'V'
 }
 
 -- The default amount of energy an actor has to spend to perform its task.
-Constants.ENERGY_THRESHOLD = 8;
+local ENERGY_THRESHOLD = 8;
 
-return Constants;
+-- Make the constants available as a module.
+return {
+    TILE_SIZE    = TILE_SIZE,
+    DIRECTION    = DIRECTION,
+    FACTIONS     = FACTIONS,
+    ACTOR_TYPES  = ACTOR_TYPES,
+    TILE_TYPES   = TILE_TYPES,
+    ACTOR_STATS  = ACTOR_STATS,
+    TILE_SPRITES = TILE_SPRITES,
+    ENERGY_THRESHOLD = ENERGY_THRESHOLD,
+};
